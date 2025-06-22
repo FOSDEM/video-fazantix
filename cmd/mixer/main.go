ad8ff2f6f27742bf1d11539f98e48b0454fe7448
Martijn Braam <martijn@brixit.nl>
2025-06-22 10:44:50 +0200

    Add pprof on port 6060

--- cmd/mixer/main.go
+++ cmd/mixer/main.go
@@ -1,7 +1,16 @@
 package main
 
 import "github.com/fosdem/fazantix/mixer"
+import _ "net/http/pprof"
+import "log"
+import "net/http"
+
+func profiler() {
+	log.Println("[pprof] Profiler on :6060")
+	log.Println(http.ListenAndServe("localhost:6060", nil))
+}
 
 func main() {
+	go profiler()
 	mixer.MakeWindowAndMix()
 }
