5161acdb0704d78f0c6fc86ebe1cce71621c027d
Martijn Braam <martijn@brixit.nl>
2025-07-17 17:39:49 +0200

    Implement warps for layer state
    Add a new warp: key for the layers in the scene that defines a starting
    state for the layer if it was previously invisible. This allows
    controlling the animation from completely unrelated scenes where
    otherwise it would fly in the layer from the top left corner.

--- lib/config/layer.go
+++ lib/config/layer.go
@@ -7,9 +7,22 @@ import (
 	yaml "github.com/goccy/go-yaml"
 )
 
+type LayerWarpCfg struct {
+	layer.LayerState
+	LayerCfgExtendedPositioning
+}
+
+type LayerWarpIntermediateState struct {
+	Warp layer.LayerState
+}
+type LayerWarpIntermediateExt struct {
+	Warp LayerCfgExtendedPositioning
+}
+
 type LayerCfg struct {
 	layer.LayerState
 	LayerCfgExtendedPositioning
+	Warp LayerWarpCfg
 }
 
 type LayerCfgExtendedPositioning struct {
@@ -22,78 +35,88 @@ type LayerCfgExtendedPositioning struct {
 }
 
 func (l *LayerCfg) Validate() error {
-	if l.X != 0 && (l.Left != 0 || l.Right != 0) {
+	err := applyExtendedPositions(&l.LayerState, &l.LayerCfgExtendedPositioning)
+	if err != nil {
+		return err
+	}
+	err = applyExtendedPositions(&l.Warp.LayerState, &l.Warp.LayerCfgExtendedPositioning)
+	return err
+}
+
+func applyExtendedPositions(l *layer.LayerState, ext *LayerCfgExtendedPositioning) error {
+	if l.X != 0 && (ext.Left != 0 || ext.Right != 0) {
 		return fmt.Errorf("cannot set both X and Left or Right for the position")
 	}
-	if l.Y != 0 && (l.Top != 0 || l.Bottom != 0) {
+	if l.Y != 0 && (ext.Top != 0 || ext.Bottom != 0) {
 		return fmt.Errorf("cannot set both Y and Top or Bottom for the position")
 	}
-	if l.Top != 0 && l.Bottom != 0 && l.Left != 0 && l.Right != 0 {
+	if ext.Top != 0 && ext.Bottom != 0 && ext.Left != 0 && ext.Right != 0 {
 		return fmt.Errorf("cannot define all four edges for position")
 	}
 
-	l.Left = normalize(l.Left, 9.0/16)
-	l.Right = normalize(l.Right, 9.0/16)
-	l.Top = normalize(l.Top, 1)
-	l.Bottom = normalize(l.Bottom, 1)
+	ext.Left = normalize(ext.Left, 9.0/16)
+	ext.Right = normalize(ext.Right, 9.0/16)
+	ext.Top = normalize(ext.Top, 1)
+	ext.Bottom = normalize(ext.Bottom, 1)
 
 	if l.Scale == 0 {
-		if l.Left != 0 && l.Right != 0 {
-			l.Scale = 1.0 - l.Left - l.Right
-		} else if l.Top != 0 && l.Bottom != 0 {
-			l.Scale = 1.0 - l.Top - l.Bottom
+		if ext.Left != 0 && ext.Right != 0 {
+			l.Scale = 1.0 - ext.Left - ext.Right
+		} else if ext.Top != 0 && ext.Bottom != 0 {
+			l.Scale = 1.0 - ext.Top - ext.Bottom
 		}
 	}
 
 	if l.X == 0 && l.Y == 0 {
-		if l.Left != 0 {
-			l.X = l.Left
+		if ext.Left != 0 {
+			l.X = ext.Left
 		} else {
-			l.X = (1.0 - l.Right) - l.Scale
+			l.X = (1.0 - ext.Right) - l.Scale
 		}
-		if l.Top != 0 {
-			l.Y = l.Top
+		if ext.Top != 0 {
+			l.Y = ext.Top
 		} else {
-			l.Y = (1.0 - l.Bottom) - l.Scale
+			l.Y = (1.0 - ext.Bottom) - l.Scale
 		}
 	}
 
-	if l.Cx != 0 {
+	if ext.Cx != 0 {
 		if l.Scale == 0 {
 			// Figure out scale from an edge constraint
-			if l.Left != 0 {
-				l.Scale = (l.Cx - l.Left) * 2
-			} else if l.Right != 0 {
-				l.Scale = ((1.0 - l.Cx) - l.Right) * 2
+			if ext.Left != 0 {
+				l.Scale = (ext.Cx - ext.Left) * 2
+			} else if ext.Right != 0 {
+				l.Scale = ((1.0 - ext.Cx) - ext.Right) * 2
 			} else {
 				return fmt.Errorf("horisontal scale undercontrained")
 			}
 		}
-		l.X = l.Cx - (l.Scale / 2)
+		l.X = ext.Cx - (l.Scale / 2)
 	}
-	if l.Cy != 0 {
+	if ext.Cy != 0 {
 		if l.Scale == 0 {
 			// Figure out scale from an edge constraint
-			if l.Top != 0 {
-				l.Scale = (l.Cy - l.Top) * 2
-			} else if l.Bottom != 0 {
-				l.Scale = ((1.0 - l.Cy) - l.Bottom) * 2
+			if ext.Top != 0 {
+				l.Scale = (ext.Cy - ext.Top) * 2
+			} else if ext.Bottom != 0 {
+				l.Scale = ((1.0 - ext.Cy) - ext.Bottom) * 2
 			} else {
 				return fmt.Errorf("vertical scale undercontrained")
 			}
 		}
-		l.Y = l.Cy - (l.Scale / 2)
+		l.Y = ext.Cy - (l.Scale / 2)
 	}
 
 	return nil
 }
 
-func (l *LayerCfg) CopyState() *layer.LayerState {
+func (l *LayerCfg) CopyState() (*layer.LayerState, *layer.LayerState) {
 	if l == nil {
-		return nil
+		return nil, nil
 	}
 	ls := l.LayerState
-	return &ls
+	ws := l.Warp.LayerState
+	return &ls, &ws
 }
 
 func (l *LayerCfg) UnmarshalYAML(b []byte) error {
@@ -107,6 +130,19 @@ func (l *LayerCfg) UnmarshalYAML(b []byte) error {
 		return err
 	}
 
+	var tempState = LayerWarpIntermediateState{}
+	var tempExt = LayerWarpIntermediateExt{}
+
+	err = yaml.Unmarshal(b, &tempState)
+	if err != nil {
+		return err
+	}
+	err = yaml.Unmarshal(b, &tempExt)
+	if err != nil {
+		return err
+	}
+	l.Warp.LayerState = tempState.Warp
+	l.Warp.LayerCfgExtendedPositioning = tempExt.Warp
 	return nil
 }
 
