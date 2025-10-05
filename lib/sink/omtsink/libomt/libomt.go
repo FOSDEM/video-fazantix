6c0afefc4e47f2512a0706f961fa4b24d22eced1
Martijn Braam <martijn@brixit.nl>
2025-10-06 00:51:42 +0200

    Implement more of the libomt sending API

--- lib/sink/omtsink/libomt/libomt.go
+++ lib/sink/omtsink/libomt/libomt.go
@@ -4,6 +4,7 @@ package libomt
 #cgo pkg-config: libomt
 #include "libomt.h"
 #include "string.h"
+#include "malloc.h"
 */
 import "C"
 import (
@@ -48,6 +49,93 @@ func (s *OmtSend) Send(frame *OmtMediaFrame, data []byte) int {
 
 	C.omt_send(s.send, s.mf)
 	return 0
+}
+
+func (s *OmtSend) SetSenderInformation(info *OmtSenderInfo) {
+	si := C.OMTSenderInfo{}
+	for i, c := range info.ProductName {
+		si.ProductName[i] = C.char(c)
+	}
+	si.ProductName[len(info.ProductName)] = 0
+	for i, c := range info.Manufacturer {
+		si.Manufacturer[i] = C.char(c)
+	}
+	si.Manufacturer[len(info.Manufacturer)] = 0
+	for i, c := range info.Version {
+		si.Version[i] = C.char(c)
+	}
+	si.Version[len(info.Version)] = 0
+
+	C.omt_send_setsenderinformation(s.send, &si)
+}
+
+func (s *OmtSend) AddConnectionMetadata(data string) {
+	C.omt_send_addconnectionmetadata(s.send, C.CString(data))
+}
+
+func (s *OmtSend) ClearConnectionMetadata() {
+	C.omt_send_clearconnectionmetadata(s.send)
+}
+
+func (s *OmtSend) SetRedirect(newAddress string) {
+	C.omt_send_setredirect(s.send, C.CString(newAddress))
+}
+
+func (s *OmtSend) GetAddress() string {
+	buffer := (*C.char)(C.malloc(1024))
+	C.omt_send_getaddress(s.send, buffer, 1024)
+	return C.GoString(buffer)
+}
+
+func (s *OmtSend) Close() {
+	C.omt_send_destroy(s.send)
+}
+
+func (s *OmtSend) Connections() int {
+	return int(C.omt_send_connections(s.send))
+}
+
+func (s *OmtSend) Receive(timeoutMilliseconds int) *OmtMediaFrame {
+	frame := C.omt_send_receive(s.send, C.int(timeoutMilliseconds))
+	if frame == nil {
+		return nil
+	}
+	result := &OmtMediaFrame{}
+	// TODO: Decode
+	return result
+}
+
+type Tally struct {
+	Preview int
+	Program int
+}
+
+func (s *OmtSend) GetTally(timeoutMilliseconds int) *Tally {
+	ts := C.OMTTally{}
+	ret := C.omt_send_gettally(s.send, C.int(timeoutMilliseconds), &ts)
+	if ret == 0 {
+		return nil
+	}
+	return &Tally{
+		Preview: int(ts.preview),
+		Program: int(ts.program),
+	}
+}
+
+func (s *OmtSend) GetVideoStatistics() OmtStatistics {
+	temp := C.OMTStatistics{}
+	C.omt_send_getvideostatistics(s.send, &temp)
+	return OmtStatistics{
+		BytesSent:              int64(temp.BytesSent),
+		BytesReceived:          int64(temp.BytesReceived),
+		BytesSentSinceLast:     int64(temp.BytesSentSinceLast),
+		BytesReceivedSinceLast: int64(temp.BytesReceivedSinceLast),
+		Frames:                 int64(temp.Frames),
+		FramesSinceLast:        int64(temp.FramesSinceLast),
+		FramesDropped:          int64(temp.FramesDropped),
+		CodecTime:              int64(temp.CodecTime),
+		CodecTimeSinceLast:     int64(temp.CodecTimeSinceLast),
+	}
 }
 
 type FrameType int
@@ -120,4 +208,22 @@ type OmtMediaFrame struct {
 	SampleRate        int
 	Channels          int
 	SamplesPerChannel int
+}
+
+type OmtSenderInfo struct {
+	ProductName  string
+	Manufacturer string
+	Version      string
+}
+
+type OmtStatistics struct {
+	BytesSent              int64
+	BytesReceived          int64
+	BytesSentSinceLast     int64
+	BytesReceivedSinceLast int64
+	Frames                 int64
+	FramesSinceLast        int64
+	FramesDropped          int64
+	CodecTime              int64
+	CodecTimeSinceLast     int64
 }
